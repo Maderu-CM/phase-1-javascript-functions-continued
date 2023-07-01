df96c9103177bd35ee44b7a490cccb9268725086
MADERU-CM <cliv.moyia@student.moringaschool.com>
2023-07-01 16:38:53 +0300

    Done Assignmemnt

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(target = "roller-skate") {
+    console.log(`This Saturday, I want to `);
+ }
+  
+  saturdayFun();
+  saturdayFun("bathe my dog");
+
+  const mondayWork = function(target = "go to the office") {
+   console.log(`This Monday, I will ${target}`);
+  };
+  
+  mondayWork();
+
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usage
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a dedicated programmer")); // Output: "You are !!!a dedicated programmer!!!"
+  
+ 
